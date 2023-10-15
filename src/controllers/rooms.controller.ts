13173d549936f7010691eefc7990287967b73ef4
Rostyslav <roctik4x@gmail.com>
2023-10-16 01:28:09 +0300

    rooms controller

---
+++ src/controllers/rooms.controller.ts
@@ -0,0 +1,28 @@
+
+require('dotenv').config(); 
+
+import { type Request, type Response } from 'express';
+import { Errors } from '../types';
+import { roomService } from './../services/room.service';
+import { Room } from './../models/room.model';
+
+class RoomsController {
+  public async createRoom(req:Request, res:Response) {
+    const name:string = String(req.body.name || '');
+    const people:number = req.body.people;
+    const roomId:number = +req.params.id;
+    
+    roomService.getRoomById(roomId).then((roomInfo) => {
+      const room = new Room(roomInfo);
+      room.addTable({ name, people }).then((table) => {
+        res.json(table);
+      }).catch(err => {
+        res.status(500).json(Errors.Unknown);
+      })
+    }).catch(err => {
+      res.status(400).json(Errors.NotExist);
+    });
+  }
+}
+
+export const roomsController = new RoomsController();
