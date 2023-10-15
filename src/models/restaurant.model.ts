0b90a63efaf38bb7d5b897d96bb16975284dc063
Rostyslav <roctik4x@gmail.com>
2023-10-15 23:03:00 +0300

    restaurant model

---
+++ src/models/restaurant.model.ts
@@ -0,0 +1,38 @@
+import { type IRestaurant, type IReservation } from './../types';
+require('dotenv').config();
+
+export default class Restaurant {
+  private _restaurant: IRestaurant;
+
+  constructor(restaurant:IRestaurant) {
+    this._restaurant = restaurant;
+  }
+
+  public getInfo():IRestaurant {
+    return this._restaurant;
+  }
+
+  public getReservations():IReservation[] {
+    let reservations:IReservation[] = [];
+
+    if (this._restaurant.rooms && this._restaurant.rooms.length) {
+      this._restaurant.rooms.forEach(room => {
+        if (room.tables && room.tables.length) {
+          room.tables.forEach((table) => {
+            if (table.reservations && table.reservations.length) {
+              table.reservations.forEach((reservation) => {
+                reservations.push(reservation);
+              });
+            }
+          });
+        }
+      });
+    }
+
+    return reservations;
+  }
+
+  public getId():number {
+    return this._restaurant.id;
+  }
+}
