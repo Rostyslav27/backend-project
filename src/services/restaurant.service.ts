57d7089ad04a8ba1cb9d1ef92f4595b03d94c817
Rostyslav <roctik4x@gmail.com>
2023-10-15 22:38:11 +0300

    restaurant service

---
+++ src/services/restaurant.service.ts
@@ -0,0 +1,50 @@
+
+import database, { RestaurantRoom, RoomTable, TableReservation, RestaurantOrganization, ReservationClient } from './../database';
+import { Model } from 'sequelize';
+import { type IRestaurant, type IRestaurantRaw } from './../types';
+require('dotenv').config();
+
+export class RestaurantService {
+  public getRestaurantById(id:number, nested?:boolean):Promise<IRestaurant> {
+    return new Promise<IRestaurant>((resolve, reject) => {
+      database.models.restaurant.findByPk<Model<IRestaurant>>(id, nested ? {
+        include: [{ 
+          association: RestaurantRoom,
+          include: [{
+            association: RoomTable,
+            include: [{
+              association: TableReservation,
+              include: [ReservationClient]
+            }]
+          }]
+        }, RestaurantOrganization]
+      } : {} ).then((restaurant) => {
+        if (restaurant) {
+          resolve(restaurant.toJSON());
+        } else {
+          reject('No restaurant')
+        }
+      }).catch(err => {
+        reject(err);
+      })
+    });
+  }
+
+  public createRestaurant(restaurant:IRestaurantRaw):Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      database.models.restaurant.create<Model<IRestaurantRaw>>({
+        name: restaurant.name
+      }).then((restaurant) => {
+        if (restaurant) {
+          resolve();
+        } else { 
+          reject('Restaurant was not created')
+        }
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const restaurantService = new RestaurantService();
