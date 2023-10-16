ff4b88544af407b3d5692fc2b5ee56a1f6ca0258
Rostyslav <roctik4x@gmail.com>
2023-10-16 19:15:44 +0300

    client service crud

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -5,6 +5,19 @@ import { type IClientRaw, type IClientFull, type IClient } from './../types';
 require('dotenv').config();
 
 export class ClientService {
+  public getFullClientById(id:number):Promise<IClientFull> {
+    return new Promise<IClientFull>((resolve, reject) => {
+      database.models.client.findByPk<Model<IClientFull>>(id).then((client) => {
+        if (client) {
+          resolve(client.toJSON());
+        } else {
+          reject('No client')
+        }
+      }).catch(err => {
+        reject(err);
+      })
+    });
+  }
 
   public createClient(client:IClientRaw, restaurantId:number):Promise<IClientFull> {
     return new Promise<IClientFull>((resolve, reject) => {
@@ -16,6 +29,7 @@ export class ClientService {
         gender: client.gender,
         email: client.email,
         phone: client.phone,
+        img: client.img,
         restaurantId: restaurantId
       }).then((client) => {
         if (client) {
@@ -24,6 +38,43 @@ export class ClientService {
         } else { 
           reject('Client was not created')
         }
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+
+  public editClient(client:IClientRaw, clientId:number):Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      database.models.client.update<Model<IClientRaw>>({
+        name: client.name,
+        surname: client.surname,
+        birthday: client.birthday,
+        note: client.note,
+        gender: client.gender,
+        email: client.email,
+        phone: client.phone,
+        img: client.img,
+      }, {
+        where: { id: clientId }
+      }).then((count) => {
+        if (count[0] > 0) {
+          resolve();
+        } else {
+          reject('client was not updated');
+        }
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+
+  public deleteClient(clientId:number):Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      database.models.client.destroy<Model<IClientRaw>>({
+        where: { id: clientId }
+      }).then(() => {
+        resolve();
       }).catch(err => {
         reject(err);
       });
