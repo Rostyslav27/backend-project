07b7fde73fb78def99d7e78354d1fbeda5e6b89c
Rostyslav <roctik4x@gmail.com>
2023-10-17 04:19:33 +0300

    table service crud

--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -5,6 +5,20 @@ import { type ITableRaw, type ITable, type ITableFull } from './../types';
 require('dotenv').config();
 
 export class TableService {
+  public getFullTableById(id:number):Promise<ITableFull> {
+    return new Promise<ITableFull>((resolve, reject) => {
+      database.models.table.findByPk<Model<ITableFull>>(id, { attributes: {exclude: ['createdAt', 'updatedAt']} }).then((table) => {
+        if (table) {
+          resolve(table.toJSON());
+        } else {
+          reject('No table');
+        }
+      }).catch(err => {
+        reject(err);
+      })
+    });
+  }
+
   public createTable(table:ITableRaw, roomId?:number):Promise<ITableFull> {
     return new Promise<ITableFull>((resolve, reject) => {
       database.models.table.create<Model<ITableRaw>>({
@@ -18,6 +32,37 @@ export class TableService {
         } else { 
           reject('Table was not created')
         }
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+
+  public editTable(tableId:number, table:ITableRaw):Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      database.models.table.update<Model<ITableRaw>>({
+        name: table.name,
+        people: table.people,
+      }, {
+        where: { id: tableId }
+      }).then((count) => {
+        if (count[0] > 0) {
+          resolve();
+        } else {
+          reject('tableId was not updated');
+        }
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+
+  public deleteTable(tableId:number):Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      database.models.table.destroy({
+        where: { id: tableId }
+      }).then(() => {
+        resolve();
       }).catch(err => {
         reject(err);
       });
